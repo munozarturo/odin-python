f2207c0f0942a264777655edd62f990e9f3842c9
munozarturo <munoz.arturoroman@gmail.com>
2022-11-26 00:25:09 -0600

    commented

--- _utils/errors.py
+++ _utils/errors.py
@@ -1,10 +1,18 @@
+"""
+Module to define custom exceptions in this module.    
+"""
+
 # Universal Errors
+
 class Error(Exception):
+    """Base class for exceptions in this module."""
     pass
 
 class ReadOnlyPropertyError(Error):
+    """Base class for errors coming from read only property deletions."""
     pass
 
 
 class MustOverrideError(Error):
+    """Base class for errors coming from methods that must be overridden."""
     pass
